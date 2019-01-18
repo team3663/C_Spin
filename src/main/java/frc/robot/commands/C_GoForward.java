7667606d35d907ea3a8a1fc3c8f7c1809eaca129
EdisonLeung <edisonleung2003@outlook.com>
2019-01-17 18:23:50 -0800

    test

---
+++ src/main/java/frc/robot/commands/C_GoForward.java
@@ -0,0 +1,57 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.ElapsedTime;
+import frc.robot.Robot;
+
+
+
+
+public class C_GoForward extends Command {
+  private final ElapsedTime timer = new ElapsedTime();
+  double speed = -.5;
+  int sec;
+
+  public C_GoForward(int sec, double speed) {
+    requires(Robot.ss_holonomicdrivetrain);
+    this.sec = sec;
+  }
+	private double deadband(double input) {
+		if (Math.abs(input) < 0.05) return 0;
+		return input;
+	}
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    timer.reset();
+    speed *= Math.abs(speed);
+    speed = deadband(speed);
+    SmartDashboard.putNumber("Forward", speed);
+    Robot.ss_holonomicdrivetrain.holonomicDrive(speed, 0, 0);
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    if(timer.getElapsedSeconds() >= sec){
+      return true;
+    }
+    return false;
+
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    Robot.ss_holonomicdrivetrain.stopDriveMotors();
+  }
+ 
+}
