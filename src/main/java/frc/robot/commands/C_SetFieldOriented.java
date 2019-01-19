2836dc0970ce2ce8e9ebd8444f6a7a2c26e95f6f
RandomPotato26 <random.potato26@gmail.com>
2019-01-19 10:50:26 -0800

    added ramping and slow command

--- src/main/java/frc/robot/commands/C_SetFieldOriented.java
+++ src/main/java/frc/robot/commands/C_SetFieldOriented.java
@@ -1,26 +1,23 @@
 package frc.robot.commands;
 
+
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 import frc.robot.subsystems.SS_HolonomicDrivetrain;
 
 public final class C_SetFieldOriented extends Command {
 
-    private final SS_HolonomicDrivetrain drivetrain;
-    private final boolean isFieldOriented;
+    private boolean mField;
 
     @Deprecated
-    public C_SetFieldOriented(SS_HolonomicDrivetrain drivetrain) {
-        this(drivetrain,true);
-    }
-
-    public C_SetFieldOriented(SS_HolonomicDrivetrain drivetrain, boolean isFieldOriented) {
-        this.drivetrain = drivetrain;
-        this.isFieldOriented = isFieldOriented;
+    public C_SetFieldOriented(boolean field) {
+        requires(Robot.ss_holonomicdrivetrain);
+        mField = field;
     }
 
     @Override
     protected void execute() {
-        drivetrain.setFieldOriented(isFieldOriented);
+        Robot.ss_holonomicdrivetrain.setFieldOriented(mField);
     }
 
     @Override
---
+++ src/main/java/frc/robot/commands/C_SlowDrivetrain.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class C_SlowDrivetrain extends Command {
+  private double mSpeed;
+  public C_SlowDrivetrain(double speed) {
+    mSpeed = speed;
+
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+    Robot.ss_holonomicdrivetrain.setSpeedMultiplier(mSpeed);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return true;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
