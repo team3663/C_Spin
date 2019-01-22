32c060377c5a5b5cfa9f48606a0bb7eb7dd47cb9
RandomPotato26 <random.potato26@gmail.com>
2019-01-21 21:01:46 -0800

    fixing my mistakes with git stash

--- src/main/java/frc/robot/commands/C_ZeroDrivetrainGyro.java
+++ src/main/java/frc/robot/commands/C_ZeroDrivetrainGyro.java
@@ -1,21 +1,20 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 import frc.robot.subsystems.SS_HolonomicDrivetrain;
 /**
  * sets gyro values to 0
  */
 
 public class C_ZeroDrivetrainGyro extends Command {
-	private SS_HolonomicDrivetrain drivetrain;
 
-	public C_ZeroDrivetrainGyro(SS_HolonomicDrivetrain drivetrain) {
-		this.drivetrain = drivetrain;
+	public C_ZeroDrivetrainGyro() {
 	}
 
 	@Override
 	public void execute() {
-		drivetrain.zeroGyro();
+		Robot.getDrivetrain().zeroGyro();
 	}
 
 	@Override
