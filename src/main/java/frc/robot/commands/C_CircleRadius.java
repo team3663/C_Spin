103a4b24c8f48aa518588a36282a03467e7d226f
EricHoelscher <erichoelscher@icloud.com>
2019-01-25 16:05:49 -0800

    add C_CircleRadius class which allows the user to specify the radius of the circle. It is currently untested

--- src/main/java/frc/robot/commands/C_CircleRadius.java
+++ src/main/java/frc/robot/commands/C_CircleRadius.java
@@ -8,27 +8,54 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 
 public class C_CircleRadius extends Command {
-  public C_CircleRadius() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
+
+  private double degree = 0;
+  private double inchesPerDegree;
+  private double arc;
+  private double speed;
+  private double radius;
+  private int mirrored = 1;
+  private int direction;
+  private double oldDistance;
+
+  public C_CircleRadius(double speed, double radius, double arc, boolean mirrored) {
+    requires(Robot.getDrivetrain());
+    this.radius = radius;
+    this.speed = speed;
+    this.arc = arc;
+    if(mirrored) {
+      this.mirrored = -1;
+    }
+    direction = (int)Math.signum(speed * arc);
+    inchesPerDegree = (Math.PI * radius * 2) / arc;
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    oldDistance = Robot.getDrivetrain().getSwerveModule(1).getDriveDistance();
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double distance = oldDistance = Robot.getDrivetrain().getSwerveModule(1).getDriveDistance();
+    double forward = Math.sin(degree) * speed * direction * mirrored;
+    double strafe = Math.cos(degree) * speed * direction;
+    Robot.getDrivetrain().holonomicDrive(forward, strafe, 0);
+    if(distance - oldDistance >= inchesPerDegree) {
+      degree++;
+      oldDistance = distance;
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return degree >= arc;
   }
 
   // Called once after isFinished returns true
@@ -40,5 +67,6 @@ public class C_CircleRadius extends Command {
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
